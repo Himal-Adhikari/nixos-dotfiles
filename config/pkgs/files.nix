bbfb6b856cfd7d9f296cda01dece38ed74779fca
Himal Adhikari <himalad321@gmail.com>
2025-11-14 02:38:24 +0545

    Generation 255

--- config/pkgs/files.nix
+++ config/pkgs/files.nix
@@ -1,19 +1,19 @@
 {pkgs, ...}:
 {
-  programs.thunar = {
-    enable = true;
-    plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-media-tags-plugin
-      thunar-volman
-    ];
-  };
+  # programs.thunar = {
+  #   enable = true;
+  #   plugins = with pkgs.xfce; [
+  #     thunar-archive-plugin
+  #     thunar-media-tags-plugin
+  #     thunar-volman
+  #   ];
+  # };
   
   environment.systemPackages = with pkgs; [
     nautilus
   ];
 
-  programs.xfconf.enable = true;
+  # programs.xfconf.enable = true;
   services.gvfs.enable = true;
-  services.tumbler.enable = true;
+  # services.tumbler.enable = true;
 }
