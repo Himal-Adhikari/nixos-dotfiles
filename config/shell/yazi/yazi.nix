710f4c1c55f88fcf264e7274caa159793dcd975a
Himal Adhikari <himalad321@gmail.com>
2025-12-01 23:41:07 +0545

    Generation 340

--- config/shell/yazi/yazi.nix
+++ config/shell/yazi/yazi.nix
@@ -4,13 +4,29 @@
     enable = true;
     shellWrapperName = "y";
     enableZshIntegration = true;
+
     flavors = {
       catppuccin_mocha = pkgs.callPackage ./catppuccin-mocha.nix { };
     };
+
     theme = {
       flavor = {
         dark = "catppuccin_mocha";
       };
+    };
+
+    plugins = {
+      smart-enter = pkgs.yaziPlugins.smart-enter;
+    };
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          run = "plugin smart-enter";
+          on = "l"; 
+          desc = "Enter the child directory, or open the file";
+        }
+      ];
     };
   };
 }
