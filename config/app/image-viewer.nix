9ae13e8bdb91b03da20ba7e7c4ba2f9cd765105a
Himal Adhikari <himalad321@gmail.com>
2026-01-12 09:24:59 +0545

    Generation 403

--- config/app/image-viewer.nix
+++ config/app/image-viewer.nix
@@ -8,11 +8,13 @@
   xdg.mimeApps = {
     enable = true;
     associations.added = {
-      "image/jpeg" = "org.gnome.Loupe.desktop";
-      "image/png" = "org.gnome.Loupe.desktop";
+      # "image/jpeg" = "org.gnome.Loupe.desktop";
+      # "image/png" = "org.gnome.Loupe.desktop";
     };
     defaultApplications = {
       "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/gif" = "org.gnome.Loupe.desktop";
+      "image/png" = "org.gnome.Loupe.desktop";
     };
   };
 }
