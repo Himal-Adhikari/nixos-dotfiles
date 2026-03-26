8f95fbeeeaab6f941c9d85b188a4b84860905f17
Himal Adhikari <himalad321@gmail.com>
2026-03-26 17:25:56 +0545

    Generation 558

--- config/hardware/firewall.nix
+++ config/hardware/firewall.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
