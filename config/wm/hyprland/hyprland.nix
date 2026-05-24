cd2833c2756d9dfc1126ac398f67caea0509af31
Himal Adhikari <himalad321@gmail.com>
2026-05-24 16:09:14 +0545

    Generation 620

--- config/wm/hyprland/hyprland.nix
+++ config/wm/hyprland/hyprland.nix
@@ -137,7 +137,7 @@
 
         # Screenshot
         "$mainMod, PRINT, exec, hyprshot -m window"
-        "PRINT, exec, hyprshot -m output"
+        ", PRINT, exec, hyprshot -m output"
         "SHIFT, PRINT, exec, hyprshot -m region"
 
         # Special Workspace
@@ -147,10 +147,6 @@
         # Browse workspace with mouse
         "$mainMod, mouse_down, workspace, e+1"
         "$mainMod, mouse_up, workspace, e-1"
-
-        # Move windows with mouse
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
 
         # Workspace Control
         "$mainMod, H, movefocus, l"
@@ -181,96 +177,25 @@
         "$mainMod SHIFT, 0, movetoworkspace, 10"
 
         # Modifier Keys
-        "XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-        "XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        "XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        "XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        "XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
-        "XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ", XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+        ", XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
 
-<<<<<<< HEAD
-$mainMod = SUPER # Sets "Windows" key as main modifier
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+      ];
 
-bind = $mainMod, RETURN, exec, $terminal
-bind = $mainMod, B, exec, $browser
-bind = $mainMod, Q, killactive,
-bind = $mainMod, T, exec, $fileManager
-bind = $mainMod, Space, togglefloating,
-bind = $mainMod, R, exec, $menu
-bind = $mainMod SHIFT, R, exec, $executable_menu
-bind = $mainMod, escape, exec, wlogout --protocol layer-shell
-bind = $mainMod, V, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy
-bind = $mainMod, F, fullscreen
-bind = $mainMod, W, exec, hypr-wallpaper
-bind = $mainMod, G, togglegroup
-bind = $mainMod SHIFT, G, moveintogroup, l
-bind = $mainMod SHIFT, G, moveintogroup, r
-bind = ALT, Tab, changegroupactive, f
-bind = ALT SHIFT, Tab, changegroupactive, b
-
-# Screenshot a window
-bind = $mainMod, PRINT, exec, hyprshot -m window
-# Screenshot a monitor
-bind = , PRINT, exec, hyprshot -m output
-# Screenshot a region
-bind = SHIFT, PRINT, exec, hyprshot -m region
-
-bind = $mainMod, S, togglespecialworkspace, magic
-bind = $mainMod SHIFT, S, movetoworkspace, special:magic
-
-bind = $mainMod, mouse_down, workspace, e+1
-bind = $mainMod, mouse_up, workspace, e-1
-
-bindm = $mainMod, mouse:272, movewindow
-bindm = $mainMod, mouse:273, resizewindow
-
-bind = $mainMod, H, movefocus, l
-bind = $mainMod, L, movefocus, r
-bind = $mainMod, K, movefocus, u
-bind = $mainMod, J, movefocus, d
-
-bind = $mainMod, 1, workspace, 1
-bind = $mainMod, 2, workspace, 2
-bind = $mainMod, 3, workspace, 3
-bind = $mainMod, 4, workspace, 4
-bind = $mainMod, 5, workspace, 5
-bind = $mainMod, 6, workspace, 6
-bind = $mainMod, 7, workspace, 7
-bind = $mainMod, 8, workspace, 8
-bind = $mainMod, 9, workspace, 9
-bind = $mainMod, 0, workspace, 10
-
-bind = $mainMod SHIFT, 1, movetoworkspace, 1
-bind = $mainMod SHIFT, 2, movetoworkspace, 2
-bind = $mainMod SHIFT, 3, movetoworkspace, 3
-bind = $mainMod SHIFT, 4, movetoworkspace, 4
-bind = $mainMod SHIFT, 5, movetoworkspace, 5
-bind = $mainMod SHIFT, 6, movetoworkspace, 6
-bind = $mainMod SHIFT, 7, movetoworkspace, 7
-bind = $mainMod SHIFT, 8, movetoworkspace, 8
-bind = $mainMod SHIFT, 9, movetoworkspace, 9
-bind = $mainMod SHIFT, 0, movetoworkspace, 10
-
-bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-bindel = ,XF86MonBrightnessUp, exec, brightnessctl set 5%+
-bindel = ,XF86MonBrightnessDown, exec, brightnessctl set 5%-
-
-bindl = , XF86AudioNext, exec, playerctl next
-bindl = , XF86AudioPause, exec, playerctl play-pause
-bindl = , XF86AudioPlay, exec, playerctl play-pause
-bindl = , XF86AudioPrev, exec, playerctl previous
-      '';
-=======
-        "XF86AudioNext, exec, playerctl next"
-        "XF86AudioPause, exec, playerctl play-pause"
-        "XF86AudioPlay, exec, playerctl play-pause"
-        "XF86AudioPrev, exec, playerctl previous"
+      bindm = [
+        # Move windows with mouse
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
       ];
     };
->>>>>>> 6cba87f (Hyprland Config migrated to be written in pure nix)
   };
 
   programs.hyprlock = {
