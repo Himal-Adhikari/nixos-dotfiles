4e44909ef8e17fc8d8bedd570c0da95a648c728f
Himal Adhikari <himalad321@gmail.com>
2026-06-24 06:54:17 +0545

    Generation 654

--- config/app/kicad.nix
+++ config/app/kicad.nix
@@ -1,6 +1,24 @@
 { pkgs, ... }:
+
+let
+  kicadThemed = pkgs.symlinkJoin {
+    name = "kicad-themed-${pkgs.kicad.version}";
+    paths = [ pkgs.kicad ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      for bin in kicad kicad-cli eeschema pcbnew gerbview pl_editor \
+                 bitmap2component pcb_calculator; do
+        if [ -e "$out/bin/$bin" ]; then
+          wrapProgram "$out/bin/$bin" \
+            --set GTK_THEME 'Adwaita:dark' \
+            --prefix XDG_DATA_DIRS : '${pkgs.gnome-themes-extra}/share'
+        fi
+      done
+    '';
+  };
+in
 {
   home.packages = [
-    pkgs.kicad
+    kicadThemed
   ];
 }
--- config/style/style.nix
+++ config/style/style.nix
@@ -1,5 +1,28 @@
 { pkgs, ... }:
 
+let
+  catppuccinKvantumMocha = pkgs.catppuccin-kvantum.override {
+    variant = "mocha";
+    accent = "blue";
+  };
+
+  catppuccinGtkMocha = pkgs.catppuccin-gtk.override {
+    variant = "mocha";
+    accents = [ "blue" ];
+    size = "standard";
+  };
+
+  qt5Palette = "${pkgs.catppuccin-qt5ct}/share/qt5ct/colors/catppuccin-mocha-blue.conf";
+  qt6Palette = "${pkgs.catppuccin-qt5ct}/share/qt6ct/colors/catppuccin-mocha-blue.conf";
+
+  qtctConf = palette: ''
+    [Appearance]
+    style=kvantum
+    custom_palette=true
+    color_scheme_path=${palette}
+    standard_dialogs=default
+  '';
+in
 {
   home.packages = with pkgs; [
     glib
@@ -14,24 +37,37 @@
 
   xdg.configFile = {
     "Kvantum/kvantum.kvconfig".text = ''
-       [General]
-       theme=catppuccin-frappe-blue
-     '';
+      [General]
+      theme=catppuccin-mocha-blue
+    '';
 
-     "Kvantum/catppuccin-frappe-blue".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/catppuccin-frappe-blue";
+    "Kvantum/catppuccin-mocha-blue".source =
+      "${catppuccinKvantumMocha}/share/Kvantum/catppuccin-mocha-blue";
+
+    "qt5ct/colors/catppuccin-mocha-blue.conf".source = qt5Palette;
+    "qt6ct/colors/catppuccin-mocha-blue.conf".source = qt6Palette;
+
+    "qt5ct/qt5ct.conf" = {
+      text = qtctConf qt5Palette;
+      force = true;
+    };
+    "qt6ct/qt6ct.conf" = {
+      text = qtctConf qt6Palette;
+      force = true;
+    };
   };
 
   gtk = {
     enable = true;
     colorScheme = "dark";
     theme = {
-      name = "catppuccin-frappe-blue-standard";
-      package = pkgs.catppuccin-gtk;
+      name = "catppuccin-mocha-blue-standard";
+      package = catppuccinGtkMocha;
     };
 
     gtk4.theme = {
-      name = "catppuccin-frappe-blue-standard";
-      package = pkgs.catppuccin-gtk;
+      name = "catppuccin-mocha-blue-standard";
+      package = catppuccinGtkMocha;
     };
   };
 }
