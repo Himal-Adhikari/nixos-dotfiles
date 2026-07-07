e80a3f93789aaabb5381a1306fbb8f505ff96840
Himal Adhikari <himalad321@gmail.com>
2026-07-07 17:55:56 +0545

    Generation 707

--- config/app/freetube.nix
+++ config/app/freetube.nix
@@ -7,6 +7,13 @@
       checkForUpdates = false;
       defaultQuality = "1080";
       baseTheme = "catppuccinMocha";
+      hideTrendingVideos = true;
+      useSponsorBlock = true;
+      sponsorBlockSelfPromo = {
+        color = "Yellow";
+        skip = "autoSkip";
+      };
+      # {"_id":"sponsorBlockSelfPromo","value":{"color":"Yellow","skip":"autoSkip"}}
     };
   };
 }
