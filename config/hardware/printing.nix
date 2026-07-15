b6ceaddd1a32e831a8fbfcac4b8237034c863cd1
Himal Adhikari <himalad321@gmail.com>
2026-07-15 15:03:13 +0545

    Generation 733

--- config/hardware/printing.nix
+++ config/hardware/printing.nix
@@ -15,6 +15,25 @@
 
   services.ipp-usb.enable = true;
 
+  hardware.printers = {
+    ensureDefaultPrinter = "USB_Printer";
+    ensurePrinters = [
+      {
+        name = "USB_Printer";
+        location = "USB (local, via ipp-usb)";
+        description = "Local USB printer";
+        deviceUri = "ipp://localhost:60000/ipp/print";
+        model = "everywhere";
+      }
+    ];
+  };
+
+  services.printing.browsedConf = ''
+    AutoClustering No
+    LocalQueueNamingIPPPrinter DNS-SD
+    BrowseFilter NOT name USB
+  '';
+
   services.avahi = {
     enable = true;
     nssmdns4 = true;
