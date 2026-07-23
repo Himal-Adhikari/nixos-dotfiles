aaafd783640d9c0f6552c1b51a0ede2675f593a7
Himal Adhikari <himalad321@gmail.com>
2026-07-23 10:35:23 +0545

    Generation 740

--- config/hardware/printing.nix
+++ config/hardware/printing.nix
@@ -2,37 +2,37 @@
 {
   services.printing = {
     enable = true;
-    drivers = with pkgs; [
-      gutenprint
-      gutenprintBin
-      hplip
-      cnijfilter2
-      epson-escpr
-      canon-cups-ufr2
-      cups-filters
-    ];
+    # drivers = with pkgs; [
+    #   gutenprint
+    #   gutenprintBin
+    #   hplip
+    #   cnijfilter2
+    #   epson-escpr
+    #   canon-cups-ufr2
+    #   cups-filters
+    # ];
   };
 
   services.ipp-usb.enable = true;
 
-  hardware.printers = {
-    ensureDefaultPrinter = "USB_Printer";
-    ensurePrinters = [
-      {
-        name = "USB_Printer";
-        location = "USB (local, via ipp-usb)";
-        description = "Local USB printer";
-        deviceUri = "ipp://localhost:60000/ipp/print";
-        model = "everywhere";
-      }
-    ];
-  };
+  # hardware.printers = {
+  #   ensureDefaultPrinter = "USB_Printer";
+  #   ensurePrinters = [
+  #     {
+  #       name = "USB_Printer";
+  #       location = "USB (local, via ipp-usb)";
+  #       description = "Local USB printer";
+  #       deviceUri = "ipp://localhost:60000/ipp/print";
+  #       model = "everywhere";
+  #     }
+  #   ];
+  # };
 
-  services.printing.browsedConf = ''
-    AutoClustering No
-    LocalQueueNamingIPPPrinter DNS-SD
-    BrowseFilter NOT name USB
-  '';
+  # services.printing.browsedConf = ''
+  #   AutoClustering No
+  #   LocalQueueNamingIPPPrinter DNS-SD
+  #   BrowseFilter NOT name USB
+  # '';
 
   services.avahi = {
     enable = true;
